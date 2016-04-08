7e913043b3f1792e89a09d3fa5c22e989de28cdd
Andrew Hoskins <andrew.hoskins16@gmail.com>
2016-04-07 21:35:18 -0600

    a decent attempt at part two but really not working yet

--- main.pl
+++ main.pl
@@ -9,7 +9,8 @@ fourSquare(N, L):-
   sumSquared(L, N).
 
 fourSquare(N, L):-
-  append(L, [X], L1),
+  append(L1, [_], L),
+  % append(L, [_], L1),
   fourSquare(N, L1).
 
 sumSquared([], 0).
@@ -27,12 +28,50 @@ ordered([X|L], Last):-
   X #>= Last,
   ordered(L, X).
 
+% Question 2
+
+disarm([], [], Out).
+disarm(A, B, Out):-
+  length(A, LenA),
+  LenA #>= 2,
+  length(B, LenB),
+  LenB #>= 1,
+
+  print(Out),
+  print(LenA),
+
+  select(ASel1, A, ARem),
+  select(ASel2, ARem, ARemRem),
+  select(BSel, B, BRem),
+  BSel #= ASel1 + ASel2,
+  append(Out1, [[ASel1, ASel2],[BSel]], Out),
+  disarm(ARemRem, BRem, Out1).
+    % TODO: label a and a1 first before select b?
 
 
-% count(L, H, X):-
-%   X #= L,
-%   X #< H.
-% count(L, H, X):-
-%   L1 #= L + 1,
-%   L1 #< H,
-%   count(L1, H, X).
+% only run this predicate if A has least 1 and B has at least 2
+disarm(A, B, Out):-
+  length(A, LenA),
+  LenA #>= 1,
+  length(B, LenB),
+  LenB #>= 2,
+
+  select(BSel1, B, BRem),
+  select(BSel2, BRem, BRemRem),
+  select(ASel, A, ARem),
+  ASel #= BSel1 + BSel2,
+  append(Out1, [[BSel1, BSel2],[ASel]], Out),
+  disarm(ARem, BRemRem, Out1).
+
+% disarm([X], 0, Out).
+% disarm(0, [X], Out).
+
+
+
+
+
+
+
+
+
+%
