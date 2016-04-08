c202e9875c33528e8cc8e49da87b691cd8e54492
Andrew Hoskins <andrew.hoskins16@gmail.com>
2016-04-07 18:19:39 -0600

    ordered foursquared but inf loop

--- main.pl
+++ main.pl
@@ -1,20 +1,14 @@
-% four squares
-% four numbers who's square sums are N
-%
-
 :- use_module(library(clpfd)).
 
+% Question 1 - Four Squares
+% TODO: problem, it's an inf loop after it gets the answers....
+
 fourSquare(N, L):-
+  ordered(L),
   length(L, 4),
   sumSquared(L, N).
+
 fourSquare(N, L):-
-  once(length(L, Len)),
-  Len #< 4,
-  sumSquared(L, Sum),
-  Sum #< N,
-  Max #= floor(sqrt(N - Sum)),
-  last(L, Last),
-  count(Last, Max, X),
   append(L, [X], L1),
   fourSquare(N, L1).
 
@@ -24,10 +18,21 @@ sumSquared([X|Tail], Sum):-
   sumSquared(Tail, Temp),
   label([X]).
 
-count(L, H, X):-
-  X #= L,
-  X #< H.
-count(L, H, X):-
-  L1 #= L + 1,
-  L1 #< H,
-  count(L1, H, X).
+ordered([]).
+ordered([X|L]):-
+  ordered(L, X).
+
+ordered([], _).
+ordered([X|L], Last):-
+  X #>= Last,
+  ordered(L, X).
+
+
+
+% count(L, H, X):-
+%   X #= L,
+%   X #< H.
+% count(L, H, X):-
+%   L1 #= L + 1,
+%   L1 #< H,
+%   count(L1, H, X).
